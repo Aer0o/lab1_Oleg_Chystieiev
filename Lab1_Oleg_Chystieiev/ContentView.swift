13f08f992e544070e892a0115509819661bbdd83
Oleg Chystieiev <oleg.chisteev@gmail.com>
2025-02-11 18:43:35 -0500

    Added timer

--- Lab1_Oleg_Chystieiev/ContentView.swift
+++ Lab1_Oleg_Chystieiev/ContentView.swift
@@ -12,6 +12,19 @@ class PrimeGameViewModel: ObservableObject {
     @Published var currentNumber = Int.random(in: 1...100)
     @Published var correctAnswers = 0
     @Published var wrongAnswers = 0
+    
+    private var timer: Timer?
+        
+        init() {
+            startTimer()
+        }
+        
+        // Start the timer that updates the number every 5 seconds
+        func startTimer() {
+            timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+                //self.updateNumber()
+            }
+        }
 }
 
 struct ContentView: View {
