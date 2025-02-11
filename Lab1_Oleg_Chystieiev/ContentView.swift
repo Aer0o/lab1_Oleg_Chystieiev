1cbe488e04fca8e320abbd3af4391468f0d0a9cf
Oleg Chystieiev <oleg.chisteev@gmail.com>
2025-02-11 18:46:25 -0500

    Added function to change the number and couple variables to handle that

--- Lab1_Oleg_Chystieiev/ContentView.swift
+++ Lab1_Oleg_Chystieiev/ContentView.swift
@@ -12,6 +12,9 @@ class PrimeGameViewModel: ObservableObject {
     @Published var currentNumber = Int.random(in: 1...100)
     @Published var correctAnswers = 0
     @Published var wrongAnswers = 0
+    @Published var showDialog = false
+    @Published var timerIsRunning = true
+    @Published var currentAttempt = 0
     
     private var timer: Timer?
         
@@ -23,6 +26,17 @@ class PrimeGameViewModel: ObservableObject {
         func startTimer() {
             timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
                 //self.updateNumber()
+            }
+        }
+    
+    // Update number every 5 seconds
+        func updateNumber() {
+            currentNumber = Int.random(in: 1...100)
+            currentAttempt += 1
+            
+            // If the user hasn't selected, mark it as wrong
+            if currentAttempt % 10 == 0 {
+                showDialog = true
             }
         }
 }
